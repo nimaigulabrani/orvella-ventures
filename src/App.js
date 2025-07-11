37489ed5f71d8c5e93a1c6637c378ec50469aee4
nimaigulabrani <nimaigulabrani@gmail.com>
2025-07-11 09:49:44 +0530

    Fixed App.js with routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
-    <div className="text-center p-8">
-      <h1 className="text-4xl font-bold">Orvella Ventures</h1>
-      <p className="text-lg mt-2">We build dreams. Coming soon...</p>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
